4e3c1f474ee22bda3e98505b7974ff090f7e3a02
Todd Roth <toddjroth@gmail.com>
2016-05-22 06:03:47 -0400

    Download by date fixed

--- app/Main.hs
+++ app/Main.hs
@@ -12,10 +12,12 @@ import           Options.Applicative
 import           System.Directory    (getHomeDirectory)
 import           System.Environment
 import           System.FilePath     (joinPath)
+import Data.Time
 
 data Operation = Operation
   { oHFile :: FilePath
   , oOFile :: FilePath
+  , oStartDate :: Maybe String
   , oVerbose :: Bool }
 
 runOp :: Parser Operation
@@ -32,14 +34,24 @@ runOp = Operation <$>
         <> value "~/.hquotes.db"
         <> metavar "QUOTES-DB FILE"
         <> help "File to save quotes, default ~/.hquotes.db" )
+    <*> optional (strOption
+      ( long "date"
+        <> short 'd'
+        <> metavar "DATE"
+        -- <> value "2016-01-01"
+        <> help "request all quotes from this date, will return \
+                 \last quote if empty format \'2013-01-01\'"))
     <*> switch
       ( long "verbose"
         <> short 'v'
         <> help "Enable verbose mode")
 
 
+symbolsFromJournal :: Journal -> [String] -- FIX THIS FUNC, TOO LONG
+symbolsFromJournal j = nub $  map acommodity  $ filter (\a -> acommodity a /= "$" && acommodity a /= "") $ concat $ map amounts $ map pamount $ concat $ map tpostings $ jtxns j
+
 getQuotesOp :: Operation -> IO ()
-getQuotesOp (Operation f o v) = do
+getQuotesOp (Operation f o sd v) = do
   homeDir <- getHomeDirectory
   if "~" `isPrefixOf` f
     then readJ (joinPath [homeDir, tail $ tail f])
@@ -52,17 +64,23 @@ getQuotesOp (Operation f o v) = do
        Left e -> putStrLn $ "Could not read specified journal file " ++ f
     writeQuotesToFile o j = do
       homeDir <- getHomeDirectory
-      let syms = nub $  map acommodity  $ filter (\a -> acommodity a /= "$" && acommodity a /= "") $ concat $ map amounts $ map pamount $ concat $ map tpostings $ jtxns j
-
-      es <- mapM (ledgerQuote v) syms
+      let syms = symbolsFromJournal j
+      es <- case sd of
+        Just d -> do
+          let startDay = parseDate d
+          mapM (ledgerQuotes v startDay) syms --(fromGregorian 2013 1 1)) syms
+        Nothing -> mapM (ledgerQuote v) syms
+      --es <- mapM (ledgerQuote v) syms
       if "~" `isPrefixOf` o
         then  mapM_ (quoteToFile (joinPath [homeDir, tail $ tail o])) es
         else  mapM_ (quoteToFile o) es
+    parseDate d = parseTimeOrError True defaultTimeLocale "%Y-%m-%d" d :: Day
+
 
 
 quoteToFile :: FilePath -> Maybe T.Text -> IO ()
 quoteToFile f q = case q of
-  Just q -> T.appendFile f (T.append "\n" q)
+  Just q -> T.appendFile f q --(T.append q "\n")
   Nothing -> return ()
 
 
--- src/Lib.hs
+++ src/Lib.hs
@@ -2,6 +2,7 @@
 
 module Lib
     ( ledgerQuote,
+     ledgerQuotes,
       toLedgerFormat,
       getLastQuote
     ) where
@@ -21,12 +22,23 @@ getLastQuote sym = do
      return $ headMay . toQuotes $ cs
    Nothing -> return Nothing
 
+getQuotes :: Symbol -> Day -> IO (Maybe [YahooQuote])
+getQuotes sym start = do
+     cd <- fmap utctDay getCurrentTime
+     mcs <- getCSV sym Daily cd (diffDays cd start)
+     case mcs of
+      Just cs ->
+        return $ Just (toQuotes cs)
+      Nothing -> return Nothing
+
+
+
 toLedgerFormat :: Symbol -> YahooQuote -> T.Text
 toLedgerFormat sym q = T.concat ["P ", yQDate q,
                                       " 12:01:00 ",
                                       T.pack sym,
                                       T.pack " $",
-                                      yQClose q]
+                                      yQClose q, "\n"]
 
 ledgerQuote :: Bool -> Symbol -> IO (Maybe T.Text)
 ledgerQuote verbose sym = do
@@ -37,3 +49,16 @@ ledgerQuote verbose sym = do
     Just q ->when verbose $ putStrLn "Success!..."
     Nothing -> when verbose $ putStrLn "Download Failed"
   return $ fmap (toLedgerFormat sym) mQ
+
+ledgerQuotes :: Bool -> Day -> Symbol -> IO (Maybe T.Text)
+ledgerQuotes verbose start sym = do
+    when verbose $
+      putStrLn $ ("Attempting to downlad quote for " ++ sym) ++ "..."
+    mQ <- getQuotes sym start
+    case mQ of
+      Just q -> do
+        when verbose $ putStrLn "Success!..."
+        return $ Just $ T.concat $ map (toLedgerFormat sym) q
+      Nothing -> do
+        when verbose $ putStrLn "Download Failed"
+        return Nothing
